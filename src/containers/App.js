20eafbe8a08226b7b56708385d3eae0aee268ca0
Julian Betancourt <julianbetancourt10@gmail.com>
2016-09-06 15:08:28 -0500

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../sass/main.sass';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hola
+      </div>
+    );
+  }
+}
+
+export default App;
